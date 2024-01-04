ae16598ece10222dd094d3ebe17cf6b4a0371a0f
steve9948 <mbuguagichimu@gmail.com>
2024-01-04 05:30:27 -0800

    Post order Traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+* binary_tree_postorder - Traverses through a binary tree using post-order method
+* @tree: pointer to root node
+* @func: pointer to a print function
+* Return:  measure the depth
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
