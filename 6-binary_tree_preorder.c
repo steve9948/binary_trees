5a456b0afea94fe2ba02495b21c03cfa115dee2f
steve9948 <mbuguagichimu@gmail.com>
2024-01-04 05:01:19 -0800

    Pre-oreder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+* binary_tree_preorder - Traverses a binary tree using pre-order traversal method
+* @tree: pointer to root node
+* @func: pointer to a print function
+* Return:  measure the depth
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
+
